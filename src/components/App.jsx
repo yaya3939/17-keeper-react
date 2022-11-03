a3150fe053d89a8d73de9cc993c95a4400f67516
yaya3939 <1220593514@qq.com>
2022-11-03 17:30:19 +0800

    complete emojipedia v1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,41 @@
 // const reactDOM = require("react-dom");
 //在import里，大小写是很重要的
 import React from "react";
-import Header from "./Header";
-import Note from "./Note";
-import Footer from "./Footer";
+import emojis from "./emojis";
+
+function Term(props) {
+  return (
+    <div className="term">
+      <dt>
+        <span className="emoji" role="img" aria-label="Tense Biceps">
+          {props.img}
+        </span>
+        <span>{props.name}</span>
+      </dt>
+      <dd>{props.meaning}</dd>
+    </div>
+  );
+}
+
+function createEmoji(Emoji) {
+  return (
+    <Term
+      key={Emoji.id}
+      img={Emoji.emoji}
+      name={Emoji.name}
+      meaning={Emoji.meaning}
+    />
+  );
+}
 
 function App() {
   return (
     <div>
-      <Header />
-      <Note />
-      <Footer />
+      <h1>
+        <span>emojipedia</span>
+      </h1>
+
+      <dl className="dictionary">{emojis.map(createEmoji)}</dl>
     </div>
   );
 }
